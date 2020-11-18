f4e70fd677c2ef4cba6cb9bc747d282dca2bc731
Alie <aboondooley@gmail.com>
2020-11-17 22:33:54 -0800

    Fully implemneted and documented. Also changed setKey to incrementKey.:

--- WordCounter.cpp
+++ WordCounter.cpp
@@ -1,29 +1,30 @@
 //
-// Created by Alie on 11/14/2020.
+// Created by Alie Boon-Dooley on 11/14/2020.
+// For Seattle University, CPSC5005, P5.HashingTheHobbit
 //
 
 #include "WordCounter.h"
-#include <iostream> // TODO remove
-using namespace std;
+#include <string>
 
 WordCounter::WordCounter(int capacity) {
     map = new LinkedList[capacity];
     uniqueWordCount = 0;
     totalWordCount = 0;
-    spacesUsed = 0;
     this->capacity = capacity;
 }
 
 WordCounter::~WordCounter() {
+    // deletes the array from The Heap
     delete[] map;
 }
 
 WordCounter::WordCounter(const WordCounter &other) {
     uniqueWordCount = other.uniqueWordCount;
     totalWordCount = other.totalWordCount;
-    spacesUsed = other.spacesUsed;
     capacity = other.capacity;
     map = new LinkedList[capacity];
+
+    // copy over all the contents
     for (int i = 0; i < capacity; i++) {
         map[i] = other.map[i];
     }
@@ -34,7 +35,6 @@ WordCounter &WordCounter::operator=(const WordCounter &rhs) {
         delete[] map;
         uniqueWordCount = rhs.uniqueWordCount;
         totalWordCount = rhs.totalWordCount;
-        spacesUsed = rhs.spacesUsed;
         capacity = rhs.capacity;
         map = new LinkedList[capacity];
         for (int i = 0; i < capacity; i++) {
@@ -45,34 +45,33 @@ WordCounter &WordCounter::operator=(const WordCounter &rhs) {
 }
 
 int WordCounter::hash(std::string word) {
-    std::hash<string> h;
+    std::hash<std::string> h;
     return h(word);
 }
 
 int WordCounter::getBucket(std::string word) const {
-    return abs(hash(word)) % capacity;
+    return abs(hash(word)) % capacity; // always positive and no greater than
+    // capacity
 }
 
 int WordCounter::addWord(std::string word) {
-    int bucket = getBucket(word);
+    int bucket = getBucket(word); // hash to get the correct bucket
     totalWordCount++;
     if (map[bucket].empty() || map[bucket].search(word) == "") {
-        if (map[bucket].empty()) {spacesUsed++;}
         map[bucket].add(1, word);
-        uniqueWordCount++;
+        uniqueWordCount++; // only increment if it's a new word
         return map[bucket].getKey(word); // always 1
     }
-    return map[bucket].setKey(word, map[bucket].getKey(word) + 1);
+    return map[bucket].incrementKey(word);
 }
 
 void WordCounter::removeWord(std::string word) {
-    int bucket = getBucket(word);
+    int bucket = getBucket(word); // hash to get the correct bucket
     if (!map[bucket].empty() && map[bucket].search(word) == word) {
         totalWordCount = totalWordCount - map[bucket].getKey(word);
         uniqueWordCount--;
         map[bucket].remove(word);
-        if (map[bucket].empty()) {spacesUsed--;}
-    }
+    }// do nothing if the word does not exist in HashMap
 }
 
 int WordCounter::getUniqueWordCount() const {
@@ -88,7 +87,7 @@ bool WordCounter::empty() const {
 }
 
 double WordCounter::getLoadFactor() const {
-    return 1.0 * spacesUsed / capacity; // multiply by double to return a double
+    return 1.0 * uniqueWordCount / capacity; // multiply by double to return a double
 }
 
 int WordCounter::getWordCount(std::string word) const {
--- WordCounter.h
+++ WordCounter.h
@@ -1,5 +1,6 @@
 //
-// Created by Alie on 11/14/2020.
+// Created by Alie Boon-Dooley on 11/14/2020.
+// For Seattle University, CPSC5005, P5.HashingTheHobbit
 //
 
 #ifndef P5_HASHINGTHEHOBBIT_WORDCOUNTER_H
@@ -9,36 +10,116 @@
 #include <string>
 
 
+/**
+ * The WordCounter Class a HashTable that takes in words and keeps
+ * track of how many times the word has been entered into the HashTable
+ * The HashTable uses an array of singly LinkedLists and Node within the
+ * LinkedList that uses a dictionary/map implementation
+ * The client can add and remove words, check how many times a given word has
+ * been entered, increment the word count of a given word as well as check
+ * the total number of words, number of unique words, and load factor. They
+ * can also check to see if the HashTable is empty
+ */
 class WordCounter {
 public:
     // BIG 4
+    /**
+     * Constructor, asks the client to supply the size
+     * @param capacity the size of the array to sue
+     */
     explicit WordCounter(int capacity);
+
+    /**
+     * Destructor - deletes from The Heap
+     */
     ~WordCounter();
+
+    /**
+     * Copy constructor
+     * Creates a deep copy
+     * @param other the WordCounter object to copy
+     */
     WordCounter(const WordCounter &other);
+
+    /**
+     * Assignment overload
+     * @param rhs the WordCounter object to copy
+     * Creates a deep copy
+     * @return myself, which now is exactly like the other WordCounter
+     */
     WordCounter &operator=(const WordCounter &rhs);
 
+
+    /**
+     * Adds a word to the Hashtable, either creates a new entry or increments
+     * the associated count if the word already exists
+     * @param word to add
+     * @return the number of times the word has been added to the HashTable
+     */
     int addWord(std::string word);
+
+    /**
+     * Removes a word completely from the HashTable (removes duplicates)
+     * @param word to remove
+     */
     void removeWord(std::string word);
+
+    /**
+     * Checks how many times the given word has been entered into the HashTable
+     * @param word the word to check
+     * @return the number of times the word has been added to the HashTable
+     * (zero if never entered)
+     */
     int getWordCount(std::string word) const;
+
+    /**
+     * Checks to see how burdened the HashTable is
+     * @return the number of unique entries divided by the size of the array
+     */
     double getLoadFactor() const;
+
+    /**
+     * Checks the number of unique words in the HashTable - does not count
+     * duplicates
+     * @return the number of unique words
+     */
     int getUniqueWordCount() const;
+
+    /**
+     * Checks the total number of words in the HashTable, including duplicate
+     * entries of the same word
+     * @return total number of entries
+     */
     int getTotalWordCount() const;
+
+    /**
+     * Checks whether the HashTable is empty
+     * @return true if empty, false if not
+     */
     bool empty() const;
 
 
 private:
+    /**
+     * Uses the std::hash function to return the hash which will always be the
+     * same for the same string
+     * @param word the string word to get the hash for
+     * @return the hash value
+     */
     static int hash(std::string word);
+
+    /**
+     * Cleans up the hash value, makes sure it is a positive number and that
+     * it is no larger than the size of the array
+     * @param word the word to pass to the hash funciton
+     * @return the bucket number
+     */
     int getBucket(std::string word) const;
 
-    int capacity;
-    int uniqueWordCount;
-    int totalWordCount;
-    int spacesUsed;
-    LinkedList *map;
-
-
-
-
+    int capacity; // size of the array
+    int uniqueWordCount; // number of words not including dups
+    int totalWordCount; // total number of words, including dups
+    LinkedList *map; // array of LinkedLists
 
 };
 
